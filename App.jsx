204ea560d1cf85bf78150d2e3eb413cc4bdc9995
AkshitaSharma@18 <sharmaakshita099@gmail.com>
2025-12-25 13:18:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Student from './Student.jsx'
+
+function App() {
+  return (
+    <> 
+    <Student name="Akshita" age={18} isStudent={true}/>
+     <Student name="Sourabh" age={25} isStudent={true}/>
+      <Student name="Astha" age={15} isStudent={true}/>
+       <Student name="Nassu" age={19} isStudent={true}/>
+       </>
+  );
+}
+
+export default App;
